123a2fb0e84c7b975aff1383ae5a3348271ae26f
aryanarora07 <140522182+aryanarora07@users.noreply.github.com>
2024-03-02 04:42:48 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,8 +18,9 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://vivavisio.vercel.app'
+  origin: 'https://vivavisio.vercel.app/create-post'
 }));
+
 app.use(express.json({ limit: '50mb' }));
 
 
