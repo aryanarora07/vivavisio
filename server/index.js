7ca0debcb47c61646e2c028e84a04499add29d7e
aryanarora07 <140522182+aryanarora07@users.noreply.github.com>
2024-03-02 05:13:41 -0800

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://vivavisio.vercel.app',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: '50mb' }));
 
