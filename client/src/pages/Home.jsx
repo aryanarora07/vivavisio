202bfa74f78bfbcf98a0d6578b80200fb04a9fcc
aryanarora07 <140522182+aryanarora07@users.noreply.github.com>
2024-03-03 06:20:05 -0800

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://vivavisiobackend.vercel.app/api/v1/post', {
+      const response = await fetch('https://vivavisiobackend.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
